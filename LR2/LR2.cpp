1b166b8f05b467e36c4a98ae890eaba2ef7c599a
Artur <artur_grishin_12345@mail.ru>
2024-09-29 20:00:19 +0700

    Completed project with automatically modified files

--- LR2/LR2.cpp
+++ LR2/LR2.cpp
@@ -4,8 +4,8 @@
 
 int main()
 {
-    patient p1{"Artur", "Grishin", "Artemovich", 2002, 0, "Barnaul sity...", "88005553535", "1111"}; // Создание записи "пациент"
-    add_patient(p1); // Добавление новой записи "пациент" в БД
-    delete_patient("Grishin"); // Удаление записи "пациент" по фамилии из БД
+    patient p1{"Ivanov", "Ivan", "Ivanovich", 2002, 0, "Barnaul_sity_Lenina46", "88005553535", "1111"}; // Создание записи "пациент"
+    //add_patient(p1); // Добавление новой записи "пациент" в БД
+    //delete_patient("Ivanov"); // Удаление записи "пациент" по фамилии из БД
     print_patient_all(); // Вывод в консоль все записи из БД "пациент"
 }
